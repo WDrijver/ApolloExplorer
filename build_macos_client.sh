3f5a0f4b3475f83fb36abe55142be13b6ba371f4
Willem Drijver <willemdrijver@mac.com>
2026-02-18 17:39:21 +0100

    Update MacOS build

--- build_macos_client.sh
+++ build_macos_client.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 clear
+/opt/homebrew/opt/qt@5/bin/qmake -recursive
 make clean
-
-/opt/homebrew/opt/qt@5/bin/qmake
 make -j16
-
 make clean
-
-sudo cp ApolloExplorerPC/ApolloExplorer /opt/ApolloExplorer/bin/ApolloExplorer
-sudo cp ApolloExplorerPC/ApolloExplorer.desktop /usr/share/applications/ApolloExplorer.desktop
